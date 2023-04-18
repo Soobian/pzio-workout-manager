878c4c759c4d8a4db9e9287b880b3491f74bfe09
Maciek <maciejciepal@gmail.com>
2023-04-18 23:14:59 +0200

    Add utils and update constants

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,8 @@
 const COLORS = {
     backgroundColor: 'rgb(250, 250, 255)',
-    accentColor: 'rgb(107, 85, 232)'
+    accentColor: 'rgb(107, 85, 232)',
+    gradientColorOne: '#a8ff78',
+    gradientColorTwo: '#78ffd6',
 }
 
-const GRADIENTS = {}
-
-export { COLORS, GRADIENTS };
+export { COLORS };
