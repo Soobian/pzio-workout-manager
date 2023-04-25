80542caa32a64d71252f5283195200b4b965e7b5
tlisowicz <tlisowicz@student.agh.edu.pl>
2023-04-25 20:49:57 +0200

    added user services

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+"""
+Plik zawiera zarejestrowany model bazy dla użytkownika
+"""
 
-# Register your models here.
+from django.contrib import admin
+from users.models import CustomUser, UserParameters
+from django.contrib.auth.admin import UserAdmin
+from django.forms import TextInput, Textarea, CharField
+from django import forms
+from django.db import models
+
+
+class UserAdminConfig(UserAdmin):
+    model = CustomUser
+    search_fields = ('email',)
+    list_filter = ('email', 'is_active', 'is_staff')
+    ordering = ('-date_joined',)
+    list_display = ('email', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 20, 'cols': 60})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(CustomUser, UserAdminConfig)
+admin.site.register(UserParameters)
